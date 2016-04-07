5fdb5b4d9f043959e6739e18a86b741b93373414
Yeongho Kim <yeonghoey@gmail.com>
2016-04-07 16:23:17 +0900

    Add initial features
    * Add initial elements
    
    * Add install, uninstall script
    
    * fixup! Add install, uninstall script
    
    * fixup! fixup! Add install, uninstall script
    
    * fixup! fixup! fixup! Add install, uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+
+set -e
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+unlink_file () {
+  local src=$1 dst=$2
+  local current="$(readlink $dst)"
+
+  if [ "$current" == "$src" ]
+  then
+    rm $dst
+    echo "$dst removed"
+  fi
+}
+
+unlink () {
+  unlink_file "$BASE_DIR/$1" "/usr/local/bin/$1"
+}
+
+unlink 'scalafmt'
+unlink 'scalafmt-added'
+unlink 'scalafmt-verify'
